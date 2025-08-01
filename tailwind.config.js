828c51730c0e1602248ad1dfb34d856ab58a78c8
genzo717 <valverde@cranealo.com>
2025-08-01 12:49:14 +0100

    Update tailwind.config.js
    new style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
-// Configuración de Tailwind CSS
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'neon-yellow': '#f7ff00'
+      },
+    },
+  },
+  plugins: [],
+};
