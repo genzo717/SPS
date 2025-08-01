b4eeff422ffcaea9793aa3185b88dea61c8e7b2f
genzo717 <valverde@cranealo.com>
2025-08-01 14:41:36 +0100

    Update getTools.js

--- src/pages/api/getTools.js
+++ src/pages/api/getTools.js
@@ -1,1 +1,8 @@
-// API que retorna herramientas desde JSON
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'src', 'data', 'tools.json');
+  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  res.status(200).json(data);
+}
